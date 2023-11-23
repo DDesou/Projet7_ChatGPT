b15636ca5043455b48ebe8100424c5c704b3f0bc
DDesou <140590021+DDesou@users.noreply.github.com>
2023-11-23 09:06:15 +0100

    Update main.py with port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
